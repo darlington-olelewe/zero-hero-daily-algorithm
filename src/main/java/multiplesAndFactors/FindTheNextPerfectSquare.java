0e9ea3e497a750f0090fe9d539413b4493056d81
win2som <obidikewinifred@yahoo.com>
2022-08-14 03:50:09 +0100

    findTheNextPerfectSquare

--- src/main/java/multiplesAndFactors/FindTheNextPerfectSquare.java
+++ src/main/java/multiplesAndFactors/FindTheNextPerfectSquare.java
@@ -18,7 +18,9 @@ package multiplesAndFactors;
 public class FindTheNextPerfectSquare {
 
     public static void main(String[] args) {
+
         System.out.println(findNextSquare(121));
+        System.out.println(findNextSquare2(121));
     }
 
     public static long findNextSquare(long sq) {
@@ -27,4 +29,35 @@ public class FindTheNextPerfectSquare {
         ++num;
         return (long)Math.pow(num,2) ;
     }
+
+    public static long findNextSquare2(long sq) {
+        double root =   Math.sqrt(sq);
+        if(root%1 != 0) return -1;
+        return (long)Math.pow(root +1, 2);
+    }
 }
+
+//a number of possible ways to check if a number is an integer
+
+//double x == 2.12;
+//
+//    if(Math.floor(x) != x){
+//        System.out.println("not an integer");
+//    } else{
+//        System.out.println("not an integer");
+//    }
+
+
+//if((x%1)!=0)
+//    {
+//        System.out.println("not a integer");
+//    }
+//    else
+//    {
+//        System.out.println("integer");
+//    }
+
+//if (x != (int)x)
+//{
+//   System.out.println("not a integer");
+
